dbee06c74d4c23b7dcf731da8227bd63297aa746
Stephanie Streeting <stephaniestreeting@gmail.com>
2026-09-18 04:42:02 +1000

    Initial Commit

---
+++ AuraSound/AuraSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraSoundApp.swift
+//  AuraSound
+//
+//  Created by Kaila Streeting on 18/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct AuraSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
